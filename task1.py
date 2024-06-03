ee80ebbe29cf98ede252d45751abbf28cca1fb5a
Sebastiaan <bas.terkeurs@gmail.com>
2024-06-03 13:30:46 -0700

    task 1 & 2

--- task1.py
+++ task1.py
@@ -1,3 +1,6 @@
+import tkinter
+from tkinter import *
+import math
 """
 ##### Task 1
 Create entry widets to allow user to enter their:
@@ -7,3 +10,39 @@ Create entry widets to allow user to enter their:
 
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
+window = Tk()
+window.geometry("400x70")
+window.title("task 1")
+
+def state():
+    name = ename.get()
+    name = str(name)
+    student = estudent.get()
+    student = str(student)
+    grade = egrade.get()
+    grade = str(grade)
+    output.config(text=f"grade = {grade}, name = {name}, student = {student}")
+
+lname = Label(window,text="name")
+lstudent = Label(window, text="student number")
+lgrade = Label(window,text="grade")
+
+ename = Entry(window)
+estudent = Entry(window)
+egrade = Entry(window)
+
+output = Label(window,text="output...",background="grey")
+button = Button(window,text="display",command=state)
+
+########
+
+lname.grid(column=1,row=1)
+lstudent.grid(column=2,row=1)
+lgrade.grid(column=3,row=1)
+ename.grid(column=1,row=2)
+estudent.grid(column=2,row=2)
+egrade.grid(column=3,row=2)
+button.grid(column=1,row=3)
+output.grid(column=2,row=3,columnspan=2)
+
+window.mainloop()
--- task2.py
+++ task2.py
@@ -1,8 +1,39 @@
 #!python3
-
+import tkinter
+from tkinter import *
+import math
+window = Tk()
+window.geometry("400x80")
+window.title("task 2")
 """
 Create a window with 3 entry widgets and 1 button.
 The first 2 entry widgets allow the user to enter in the 2 short sides of a right triangle.
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+
+def caclulate():
+    l = elength.get()
+    l = float(l)
+    w = ewidth.get()
+    w = float(w)
+    h = (w**2 + l**2)**0.5
+    h = round(h,2)
+    h = str(h)
+    eoutput.config(text=h)
+    
+llength=Label(window,text="length")
+lwidth=Label(window,text="width")
+elength=Entry(window)
+ewidth=Entry(window)
+eoutput=Label(window,text="             hypotenuse         ",background="grey")
+button=Button(window,text="calculate",command=caclulate)
+
+llength.grid(row=1,column=1)
+lwidth.grid(row=1,column=2)
+elength.grid(row=2,column=1)
+ewidth.grid(row=2,column=2)
+eoutput.grid(row=2,column=3)
+button.grid(row=3,column=3)
+
+window.mainloop()
